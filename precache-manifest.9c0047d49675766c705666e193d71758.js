d6ad17b66e701d0600ef4ae827e63b7f11c053ce
Kiryl Matusevich <iamkirylm@gmail.com>
2019-07-09 20:24:46 +0200

    Updates

---
+++ precache-manifest.9c0047d49675766c705666e193d71758.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12e1c5506516d1dc22078d9ca6095ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e876b6bf7f46589a865",
+    "url": "/static/css/main.bcc979a3.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "2e876b6bf7f46589a865",
+    "url": "/static/js/main.6d471c5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
